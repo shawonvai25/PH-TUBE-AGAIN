bc8ad5a436754e2e743bea602fedcf1267c3f36a
shawonvai25 <abdullahalshawon25@gmail.com>
2025-01-23 03:39:54 +0600

    added

--- scripts/video.js
+++ scripts/video.js
@@ -12,6 +12,46 @@ const loadCategories = () => {
     .catch(error => console.log(error))
 };
 
+const loadVideos = () => {
+  
+    //fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error))
+};
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("videos");
+  videos.forEach( video =>{
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList ="card card-compact";
+    card.innerHTML = `
+    <figure class="h-[200px] rounded-md">
+    <img class="h-full w-full object-cover"
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2 ">
+   <div>
+   <img class="object-cover w-10 h-10 rounded-full" src=${video.authors[0].profile_picture} />
+   </div>
+   <div>
+   <h2 class="font-semibold">${video.title}</h2>
+    <div class="flex items-center gap-2">
+    <p class="text-gray-400">${video.authors[0].profile_name}</p>
+    <img class="w-5 " src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" />
+    </div>
+  
+   <p></p>
+   </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  });
+};
+
 //createDisplayCategories
 const displayCategories  = (categories) => {
    const categoryContainer = document.getElementById("categories");
@@ -30,3 +70,4 @@ const displayCategories  = (categories) => {
 };
 
 loadCategories();
+loadVideos();
