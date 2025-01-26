d6a2c1b361c3c6b59675ba2ca84ed22a3df1cd9a
shawonvai25 <abdullahalshawon25@gmail.com>
2025-01-26 11:49:36 +0600

    added

--- scripts/video.js
+++ scripts/video.js
@@ -29,8 +29,8 @@ const removeActiveClass = () => {
    .catch(error => console.log(error));
  };
 
-  const loadVideos = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+  const loadVideos = (searchText = "") => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then( res => res.json())
     .then(data => displayVideos(data.videos))
     .catch(error => console.log(error));
@@ -151,6 +151,12 @@ const displayCategories = (categories) => {
  });
  
 };
+
+document.getElementById("search-input").addEventListener("keyup", (e) =>{
+   loadVideos(e.target.value);
+}) ;
+
+
 
 
 loadCategories();
